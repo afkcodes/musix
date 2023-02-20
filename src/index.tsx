a26d3997a5219c27b55aa364913a63a39c28a596
Ashish Kumar <ashish.b.kumar@rakuten.com>
2023-02-20 19:35:26 +0530

    add react router & page transition

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
+import PageContainer from './Containers/PageContainer';
 import './index.css';
-import Home from './pages/Home';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
 
-root.render(<Home />);
+root.render(
+  <BrowserRouter>
+    <PageContainer />
+  </BrowserRouter>
+);
